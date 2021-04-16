890771c3403d315194ae90c03149af11c2f82385
Francois Polo <andasan@gmail.com>
2021-04-16 10:04:07 -0700

    added quantity reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,18 +4,41 @@ import { connect } from 'react-redux'
 import './cart-icon.styles.scss'
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
-
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
-const CartIcon = ({toggleCartHiddenProps}) => (
+const CartIcon = ({toggleCartHiddenProps, itemCount}) => (
     <div className="cart-icon">
         <ShoppingIcon className="shopping-icon" onClick={toggleCartHiddenProps} />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
+
+const mapStateToProps = ({ cart: { cartItems }}) => ({
+    itemCount: cartItems.reduce((acc, cartItem) => ( acc + cartItem.quantity), 0)
+})
 
 const mapDispatchToProps = (dispatch) => ({ 
     toggleCartHiddenProps: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
+
+// //summing an array of numbers
+// function sumOfQuantity(arr){
+//     let sum = 0;
+//     for(const item of arr){
+//         sum += item.quantity
+//     }
+//     return sum
+// }
+
+// sumOfQuantity([{id:1, quantity: 2}, {id:2, quantity: 5}]) //7
+
+//reduce function
+// function sumOfQuantity(arr){
+//     const reducer = (sum, item) => sum + item.quantity
+//     const initVal = 0;
+//     return arr.reduce(reducer, initVal)
+// }
+
+// sumOfQuantity([{id:1, quantity: 2}, {id:2, quantity: 5}]) //7
