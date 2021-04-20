090856377faec76810cb17a63fc1263d11e4c2d9
Francois Polo <andasan@gmail.com>
2021-04-20 11:05:21 -0700

    added checkout-item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,11 +1,23 @@
 //action types
 export const ADD_CART_ITEM = 'ADD_CART_ITEM'
+export const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM'
+export const CLEAR_FROM_CART = 'CLEAR_FROM_CART'
 export const TOGGLE_CART_HIDDEN = 'TOGGLE_CART_HIDDEN'
 
 
 //action creators
 export const addItem = (item) => ({
     type: ADD_CART_ITEM,
+    payload: item
+})
+
+export const removeItem = (item) => ({
+    type: REMOVE_CART_ITEM,
+    payload: item
+})
+
+export const clearItemFromCart = (item) => ({
+    type: CLEAR_FROM_CART,
     payload: item
 })
 
