78891358db6bd4c8ede047b848b95112410a7a1e
Francois Polo <andasan@gmail.com>
2021-04-21 11:17:48 -0700

    added query requests from components (collection page and overview)

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react' 
+
+import { CollectionsOverviewContainer } from './collection-overview.styles'
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+const CollectionOverview = ({ collections }) => (
+    <CollectionsOverviewContainer>
+    {
+        collections && collections.map(({ id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))
+    }
+    </CollectionsOverviewContainer>
+)
+
+export default CollectionOverview
--- src/components/collection-overview/collection-overview.container.jsx
+++ src/components/collection-overview/collection-overview.container.jsx
@@ -1,23 +1,55 @@
-import {connect} from 'react-redux'
-import {compose} from 'redux'
-import { createStructuredSelector} from 'reselect'
+import { useQuery, gql } from '@apollo/client'
 
-import WithSpinner from '../withSpinner/with-spinner.component';
-import CollectionOverview from './collections-overview.component';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selector'
+import Spinner from '../spinner/spinner.component';
+import CollectionOverview from './collection-overview.component'
 
-// const mapStateToProps = state => ({
-//     isLoading: state.shop.isFetching
-// })
+const GET_COLLECTIONS = gql`
+    {
+        collections {
+            id
+            title
+            items {
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }
+`
 
-const mapStateToProps = createStructuredSelector({
-    isLoading: selectIsCollectionFetching
-})
+const CollectionOverviewContainer = () => {
+    const { loading, error, data } = useQuery(GET_COLLECTIONS)
 
-const CollectionOverviewContainer = compose(
-    connect(mapStateToProps),
-    WithSpinner
-)(CollectionOverview)
+    if(loading) return <Spinner />
+    if(error) return <p>Error :(</p>
 
+    return <CollectionOverview collections={data.collections} />
+}
 
 export default CollectionOverviewContainer
+
+
+// import {connect} from 'react-redux'
+// import {compose} from 'redux'
+// import { createStructuredSelector} from 'reselect'
+
+// import WithSpinner from '../withSpinner/with-spinner.component';
+// import CollectionOverview from './collections-overview.component';
+// import { selectIsCollectionFetching } from '../../redux/shop/shop.selector'
+
+// // const mapStateToProps = state => ({
+// //     isLoading: state.shop.isFetching
+// // })
+
+// const mapStateToProps = createStructuredSelector({
+//     isLoading: selectIsCollectionFetching
+// })
+
+// const CollectionOverviewContainer = compose(
+//     connect(mapStateToProps),
+//     WithSpinner
+// )(CollectionOverview)
+
+
+// export default CollectionOverviewContainer
